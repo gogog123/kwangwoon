a5637d716ea3a138c43954c2096b0f23d0490533
sejung-hong <sejungh@naver.com>
2021-02-25 21:21:08 +0900

    객프 1차

---
+++ 3-1(2020)/1차/assignment1-6.cpp
@@ -0,0 +1,150 @@
+﻿#include <iostream>
+using namespace std;
+
+void Game_board(int location[3][3])
+{
+	cout << " X  0   1   2" << endl;
+	cout << "Y  --- --- ---" << endl;
+	for (int row = 0; row < 3; row++)
+	{
+		cout << row << " | ";
+		for (int col = 0; col < 3; col++)
+		{
+			if (location[row][col] == 1) //player 1
+			{
+				cout << "o";
+			}
+			else if (location[row][col] == 2) //player 2
+			{
+				cout << "x";
+			}
+			else
+				cout << " ";
+			cout << " | ";
+		}
+		cout << endl;
+		cout << "   --- --- ---" << endl;
+	}
+}//Print Game board
+
+bool  Winner(int location[][3])
+{
+	for (int i = 0; i < 3; i++)
+	{
+		if (location[0][i] != 0 && location[0][i] == location[1][i] && location[1][i] == location[2][i])
+		{
+			if (location[0][i] == 1)
+			{
+				cout << "Winner is [Player 1]" << endl;
+			}
+			else if (location[0][i] == 2)
+			{
+				cout << "Winner is [Player 2]" << endl;
+			}
+			return true;
+		} // column bingo.
+
+		else if (location[i][0] != 0 && location[i][0] == location[i][1] && location[i][1] == location[i][2])
+		{
+			if (location[i][0] == 1)
+			{
+				cout << "Winner is [Player 1]" << endl;
+			}
+			else if (location[i][0] == 2)
+			{
+				cout << "Winner is [Player 2]" << endl;
+			}
+			return true;
+		} // row bingo.
+	}
+
+	if (location[0][0] != 0 && location[0][0] == location[1][1] && location[1][1] == location[2][2])
+	{
+		if (location[0][0] == 1)
+		{
+			cout << "Winner is [Player 1]" << endl;
+		}
+		else if (location[0][0] == 2)
+		{
+			cout << "Winner is [Player 2]" << endl;
+		}
+		return true;
+	} //diagonal bingo.
+
+
+	if (location[0][2] != 0 && location[0][2] == location[1][1] && location[1][1] == location[2][0])
+	{
+		if (location[0][2] == 1)
+		{
+			cout << "Winner is [Player 1]" << endl;
+		}
+		else if (location[0][2] == 2)
+		{
+			cout << "Winner is [Player 2]" << endl;
+		}
+		return true;
+	}//diagonal bingo.
+
+	return false; // false bigo
+}
+
+int main()
+{
+	int x, y;
+	int count = 0;
+	int location[3][3] = { 0 };
+	bool result;
+
+	Game_board(location); //Print Game board
+
+
+	while (1)
+	{
+		while (1)
+		{
+			cout << "[Play 1] Enter your location [x y] : ";
+			cin >> x >> y;
+			if (x > 2 || y > 2 || location[x][y] != 0) //input is wrong
+			{
+				cout << "Please input again\n\n";
+			}
+			else
+				break;
+		}
+		location[y][x] = 1;
+		cout << endl;
+		Game_board(location);
+		if (Winner(location) == true) //check the winner
+			break;
+		count++;
+
+		if (count == 9)
+		{
+			cout << "Draw";
+			break;
+		} //when drawing.
+
+		while (1)
+		{
+			cout << "[Play 2] Enter your location [x y] : ";
+			cin >> x >> y;
+			if (x > 2 || y > 2 || location[x][y] != 0) //input is wrong
+			{
+				cout << "Please input again\n\n";
+			}
+			else
+				break;
+		}
+
+		location[y][x] = 2;
+		cout << endl;
+		Game_board(location);
+		if (Winner(location) == true) //check the winner
+			break;
+		count++;
+	}
+
+	cout << endl;
+
+	return 0;
+}
---
+++ 3-1(2020)/1차/assignment1-7.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int p1_x, p1_y;
+	int p2_x, p2_y;
+	int p3_x, p3_y;
+
+	while (1)
+	{
+		cout << "P1: ";
+		cin >> p1_x >> p1_y;
+		cout << "P2: ";
+		cin >> p2_x >> p2_y;
+		cout << "P3: ";
+		cin >> p3_x >> p3_y;
+		if ((p1_x != p2_x || p1_y != p2_y)
+			&& (p1_x != p3_x || p1_y != p3_y)
+			&& (p2_x != p3_x || p2_y != p3_y))
+			break; //exception
+		cout << "Please input different number\n\n";
+	}
+
+	int p1_to_p2_x = p2_x - p1_x; //AB vector x
+	int p1_to_p2_y = p2_y - p1_y; //AB vector y
+	int p1_to_p3_x = p3_x - p1_x; //AC vector x
+	int p1_to_p3_y = p3_y - p1_y; //AC vector y
+	
+	int check1 = p1_to_p2_x * p1_to_p3_y;
+	int check2 = p1_to_p3_x * p1_to_p2_y;
+	int cross_product = (p1_to_p2_x * p1_to_p3_y) - (p1_to_p3_x * p1_to_p2_y);
+	
+	//value cross product
+
+	if (cross_product < 0) // when negative clockwise
+		cout << "Clockwise" << endl;
+	else if (cross_product > 0) // when positive counter clockwise
+		cout << "Counter-Clockwise" << endl;
+	else // when zero straight
+		cout << "Straight" << endl;
+
+	return 0;
+}
